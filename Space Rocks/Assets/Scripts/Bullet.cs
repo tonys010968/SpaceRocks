d1622e84617b26689b891c112b1c48399d7e4d8f
Jacyn <anthony@thesavocas.com>
2023-01-17 20:32:50 -0500

    Added Bullet and Shooting

---
+++ Space Rocks/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 500.0f;
+    public float maxLifetime = 10.0f;
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Project(Vector2 direction)
+    {
+        _rigidBody.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Space Rocks/Assets/Scripts/Player.cs
+++ Space Rocks/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     public float thrustSpeed = 1.0f;
     public float turnSpeed = 1.0f;
     private Rigidbody2D _rigidBody;
@@ -30,6 +31,11 @@ public class Player : MonoBehaviour
         {
             _turnDirection = 0.0f;  
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     private void FixedUpdate()
@@ -42,5 +48,11 @@ public class Player : MonoBehaviour
         {
             _rigidBody.AddTorque(_turnDirection * this.turnSpeed);
         }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
     }
 }
