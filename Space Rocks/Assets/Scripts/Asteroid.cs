3dc5cc29efa89bb3f131ec6cce013ad628cc801c
Jacyn <anthony@thesavocas.com>
2023-01-22 20:11:53 -0500

    Rocks can get destroyed

--- Space Rocks/Assets/Scripts/Asteroid.cs
+++ Space Rocks/Assets/Scripts/Asteroid.cs
@@ -37,4 +37,29 @@ public class Asteroid : MonoBehaviour
 
         Destroy(this.gameObject, this.maxLifetime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            if ((this.size * 0.5f) >= this.minSize)
+            {
+                CreateSplit();
+                CreateSplit();
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void CreateSplit()
+    {
+        Vector2 position = this.transform.position;
+        position += Random.insideUnitCircle * 0.5f;
+
+        Asteroid half = Instantiate(this, position, this.transform.rotation);
+        half.size = this.size * 0.5f;
+        half.SetTrajectory(Random.insideUnitCircle.normalized *this.speed);
+    }
+
 }
